c97f6fb0c22d79edcf547820e9ef00c7b1edf373
axel tavares <tavaresaxel95@gmail.com>
2024-02-01 20:48:14 -0500

    saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ var searchInput = document.getElementById("search-input")
 var searchButton = document.getElementById("search-btn")
 
 async function searchHotel(){
-    const url = 'https://booking-com13.p.rapidapi.com/languages';
+    const url = 'https://imdb146.p.rapidapi.com/v1/find/?query=brad';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '3769f18ce9mshc4905c5b91696b2p16c85ejsnadaefda5f6b2',
-		'X-RapidAPI-Host': 'booking-com13.p.rapidapi.com'
+		'X-RapidAPI-Host': 'imdb146.p.rapidapi.com'
 	}
 };
 
@@ -17,7 +17,10 @@ try {
 	console.log(result);
 } catch (error) {
 	console.error(error);
+
+
 }
 }
+
 
 searchButton.addEventListener("click", searchHotel)
